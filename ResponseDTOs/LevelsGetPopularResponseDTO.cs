cb08312ca7673e0d13975df384fa0571f6dae166
Christiaan Bloemendaal <github@tnrd.net>
2023-04-18 22:01:22 +0200

    feat: add levels/popular response DTO

---
+++ ResponseDTOs/LevelsGetPopularResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
+
+public class LevelsGetPopularResponseDTO
+{
+    public class Info
+    {
+        public int LevelId { get; set; }
+        public string LevelName { get; set; }
+        public int RecordsCount { get; set; }
+    }
+    
+    public List<Info> Levels { get; set; }
+}
