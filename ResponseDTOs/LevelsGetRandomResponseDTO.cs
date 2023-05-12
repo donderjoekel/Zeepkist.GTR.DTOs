acb1ff62c450d140e7b3d608709f8364aac57080
Christiaan Bloemendaal <github@tnrd.net>
2023-05-12 17:27:07 +0200

    feat: add LevelsGetRandomResponseDTO

---
+++ ResponseDTOs/LevelsGetRandomResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
+
+namespace TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
+
+public class LevelsGetRandomResponseDTO
+{
+    public List<LevelResponseModel> Levels { get; set; } = null!;
+}
