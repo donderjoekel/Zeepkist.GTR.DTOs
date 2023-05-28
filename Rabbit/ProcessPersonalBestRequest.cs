28855592369e0698c301ce8d39d3e088f0203222
Christiaan Bloemendaal <github@tnrd.net>
2023-05-28 19:59:17 +0200

    feat: add ProcessPersonalBestRequest

---
+++ Rabbit/ProcessPersonalBestRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TNRD.Zeepkist.GTR.DTOs.Rabbit;
+
+public class ProcessPersonalBestRequest
+{
+    public int Id { get; set; }
+    public int Level { get; set; }
+    public float Time { get; set; }
+}
