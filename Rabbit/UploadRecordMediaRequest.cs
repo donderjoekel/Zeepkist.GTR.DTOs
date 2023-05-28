f319408238c89523affd884047d73f4180500435
Christiaan Bloemendaal <github@tnrd.net>
2023-05-28 16:08:34 +0200

    feat: add UploadRecordMediaRequest

---
+++ Rabbit/UploadRecordMediaRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TNRD.Zeepkist.GTR.DTOs.Rabbit;
+
+public class UploadRecordMediaRequest
+{
+    public int Id { get; set; }
+    public string GhostData { get; set; } = null!;
+    public string ScreenshotData { get; set; } = null!;
+}
