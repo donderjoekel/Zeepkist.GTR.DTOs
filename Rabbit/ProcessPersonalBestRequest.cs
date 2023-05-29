2908e9189c5c00256ef4e0da1870d9fc3a1f7c23
Christiaan Bloemendaal <github@tnrd.net>
2023-05-29 14:05:48 +0200

    feat: add Record id to ProcessPersonalBestRequest

--- Rabbit/ProcessPersonalBestRequest.cs
+++ Rabbit/ProcessPersonalBestRequest.cs
@@ -2,6 +2,7 @@
 
 public class ProcessPersonalBestRequest
 {
+    public int Record { get; set; }
     public int User { get; set; }
     public int Level { get; set; }
     public float Time { get; set; }
