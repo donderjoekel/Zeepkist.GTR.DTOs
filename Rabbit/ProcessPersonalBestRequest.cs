8dd5dc08699e537d2ad41dd88ade287ce6f38411
Christiaan Bloemendaal <github@tnrd.net>
2023-05-29 12:11:41 +0200

    refactor: change Id to User in ProcessPersonalBestRequest

--- Rabbit/ProcessPersonalBestRequest.cs
+++ Rabbit/ProcessPersonalBestRequest.cs
@@ -2,7 +2,7 @@
 
 public class ProcessPersonalBestRequest
 {
-    public int Id { get; set; }
+    public int User { get; set; }
     public int Level { get; set; }
     public float Time { get; set; }
 }
