abf320cc42994d4b4571fe55bd651df10cd65932
Christiaan Bloemendaal <github@tnrd.net>
2023-05-29 19:53:37 +0200

    feat: add ProcessWorldRecordRequest

---
+++ Rabbit/ProcessWorldRecordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TNRD.Zeepkist.GTR.DTOs.Rabbit;
+
+public class ProcessWorldRecordRequest
+{
+    public int Record { get; set; }
+    public int User { get; set; }
+    public int Level { get; set; }
+    public float Time { get; set; }
+}
