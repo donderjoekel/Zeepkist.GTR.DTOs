6888fd82250c1731a85e1b0a4336dc7ee1a4d21b
Christiaan Bloemendaal <github@tnrd.net>
2023-08-19 15:05:56 +0200

    feat: add stats get request dto

---
+++ RequestDTOs/StatsGetRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
+
+public class StatsGetRequestDTO
+{
+    [QueryParam] public int UserId { get; set; }
+    [QueryParam] [DefaultValue(null)] public int? Month { get; set; }
+    [QueryParam] [DefaultValue(null)] public int? Year { get; set; }
+}
