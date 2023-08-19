cfab124d1a365a45986d58504699c7e468c849c5
Christiaan Bloemendaal <github@tnrd.net>
2023-08-19 15:42:34 +0200

    feat: add stats aggregate request dto

---
+++ RequestDTOs/StatsAggregateRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
+
+public class StatsAggregateRequestDTO
+{
+    [QueryParam] [DefaultValue(null)] public int? Month { get; set; }
+    [QueryParam] [DefaultValue(null)] public int? Year { get; set; }
+}
