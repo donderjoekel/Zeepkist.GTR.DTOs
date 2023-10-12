a9eda26b2a458693e5e86def373f01208987cee5
Christiaan Bloemendaal <github@tnrd.net>
2023-10-12 12:02:17 +0200

    fix: personal best by record

--- ResponseDTOs/PersonalBestGetByRecordResponseDTO.cs
+++ ResponseDTOs/PersonalBestGetByRecordResponseDTO.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
+﻿using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
 
 namespace TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
 
 public class PersonalBestGetByRecordResponseDTO
 {
-    public int TotalAmount { get; set; }
-    public List<PersonalBestResponseModel> Items { get; set; } = null!;
+    public PersonalBestResponseModel PersonalBest { get; set; } = null!;
 }
