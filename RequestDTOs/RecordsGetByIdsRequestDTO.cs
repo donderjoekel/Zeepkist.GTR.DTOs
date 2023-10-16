91ed2a9ba0f7705976001d6c30885785b5d383f1
Christiaan Bloemendaal <github@tnrd.net>
2023-10-16 15:44:55 +0200

    fix: add auto generate builder attribute

--- RequestDTOs/RecordsGetByIdsRequestDTO.cs
+++ RequestDTOs/RecordsGetByIdsRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿namespace TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
 
+[AutoGenerateBuilder]
 public class RecordsGetByIdsRequestDTO : GenericGetRequestDTO
 {
     public int[] Ids { get; set; } = null!;
