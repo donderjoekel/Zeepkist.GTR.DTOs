fe2f42fe2f3f09278c039efb08da76aa45e45e45
Christiaan Bloemendaal <github@tnrd.net>
2023-10-17 10:34:31 +0200

    feat: add leaderboard DTOs

---
+++ RequestDTOs/PersonalBestGetLeaderboardRequestDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
+
+[AutoGenerateBuilder]
+public class PersonalBestGetLeaderboardRequestDTO : GenericGetRequestDTO
+{
+    public string Level { get; set; } = null!;
+}
---
+++ ResponseDTOs/PersonalBestGetLeaderboardResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
+
+namespace TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
+
+public class PersonalBestGetLeaderboardResponseDTO
+{
+    public class Item
+    {
+        public RecordResponseModel Record { get; set; } = null!;
+        public UserResponseModel User { get; set; } = null!;
+    }
+
+    public List<Item> Items { get; set; } = null!;
+}
