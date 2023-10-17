721d9568db095b12120bfb4634667681b573a134
Christiaan Bloemendaal <github@tnrd.net>
2023-10-17 10:08:24 +0200

    feat: add request and response DTOs for ui info

---
+++ RequestDTOs/PersonalBestGetUiRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
+
+[AutoGenerateBuilder]
+public class PersonalBestGetUiRequestDTO
+{
+    public string Level { get; set; } = null!;
+    public int User { get; set; }
+}
---
+++ ResponseDTOs/PersonalBestGetUiResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿using TNRD.Zeepkist.GTR.DTOs.ResponseModels;
+
+namespace TNRD.Zeepkist.GTR.DTOs.ResponseDTOs;
+
+public class PersonalBestGetUiResponseDTO
+{
+    public RecordResponseModel Record { get; set; } = null!;
+    public UserResponseModel User { get; set; } = null!;
+}
