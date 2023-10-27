9bee26aa2ffb6caed4d167fc5b1323d43c3faddc
Christiaan Bloemendaal <github@tnrd.net>
2023-10-27 15:55:09 +0200

    feat: added PlayerPoints

---
+++ ResponseModels/PlayerPointsResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TNRD.Zeepkist.GTR.DTOs.ResponseModels;
+
+public class PlayerPointsResponseModel
+{
+    public int Id { get; set; }
+    public int User { get; set; }
+    public int Rank { get; set; }
+    public int Points { get; set; }
+}
