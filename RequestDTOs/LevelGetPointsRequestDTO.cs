6e61c3842681575c76b2727245d5486c234c72e9
Christiaan Bloemendaal <github@tnrd.net>
2024-02-05 20:18:41 +0100

    feat: add LevelGetPoints DTO

---
+++ RequestDTOs/LevelGetPointsRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace TNRD.Zeepkist.GTR.DTOs.RequestDTOs;
+
+[AutoGenerateBuilder]
+public class LevelGetPointsRequestDTO : GenericGetRequestDTO
+{
+    [QueryParam] [DefaultValue(false)] public bool? SortByPoints { get; set; }
+    [QueryParam] [DefaultValue(false)] public bool? Ascending { get; set; }
+}
